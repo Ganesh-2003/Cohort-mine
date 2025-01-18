7927846c929491ed5d21d726f7395c0b7124ef93
Ganesh Puppala <74791350+Ganesh-2003@users.noreply.github.com>
2025-01-19 00:13:54 +0530

    intial commit

--- week-2/express-class/index.js
+++ week-2/express-class/index.js
@@ -1,24 +1,11 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+var users = [{
+    name:"John",
+    kidneys:[{
+        healthy:false
+    },{
+        healthy:true
+    }]
+}]
 
-function sum(n)
-{
-    let sum = 0;
-    for(let i = 0;i<=n;i++)
-    {
-        sum = sum+i;
-    }
-    return sum;
-}
-
-
-app.get("/basic-server",function(req,res){
-    const input = req.query.input;
-    const output = sum(input);
-    res.send("Ouput of Sum of Number till N is : " + output);
-})
-
-app.listen(port,()=>{
-    console.log("Listening on Port : "+port);
-})  
+console.log(users[0]);
+console.log(users[1]);
---
+++ week-2/express-class/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+console.log(users[0]);
+console,log(users[1]);
