348982f572d046439526d0a852dec749e52a9cb7
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-02-06 08:01:31 +0530

    initial commit

---
+++ week-3/01-middlewares/middlewarepractice/zodpractice.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const zod = require('zod');
+
+const app = express();
+
+app.use(express.json());
+
+const schema = zod.array(zod.number());
+
+app.post("/health-checkup" ,function(req,res){
+    const kidneys = req.body.kidneys;
+
+    const response = schema.safeParse(kidneys)
+
+    if(!response.success)
+    {
+        res.status(411).send({
+            msg:"Invalid Input"
+        })
+    }
+
+    res.send({
+        response
+    })
+});
+
+app.listen(3000);
