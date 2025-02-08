e75df3b41cc7ace87f0afe5f4c0a6b14f1fafc75
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-02-08 17:08:29 +0530

    intial commit

--- week-3/3.2 Database_Auth/mongoose.js
+++ week-3/3.2 Database_Auth/mongoose.js
@@ -1,13 +1,45 @@
+const express = require('express');
+
+const app = express();
+
 const mongoose = require('mongoose');
 
 mongoose.connect("mongodb+srv://ganeshpuppala:Ganesh2003@cluster0.ga8zi.mongodb.net/");
 
 const User = mongoose.model('users',{name: String, email: String, password: String});
 
-const user = new User({
-    name: 'Ganesh Puppala',
-    email: 'ganesh@gmail.com',
-    password: '123456'
-})
+app.use(express.json());
 
-user.save();
+app.post("/signin", async function(req, res) {
+
+    const username = req.body.username;
+    const password = req.body.password;
+    const name = req.body.name;
+
+    const existingUser = await User.findOne({email: username});
+
+    if(existingUser)
+    {
+        return res.status(400).json({
+            msg:"User Already Existing in Database Please Sign in"
+        })
+    }
+
+    const user = new User({
+        name: name,
+        email: username,
+        password: password
+    })
+
+    user.save();
+
+    res.json({
+        msg: "User Successfully Created"
+    })
+});
+
+app.listen(3000);
+
+
+
+
