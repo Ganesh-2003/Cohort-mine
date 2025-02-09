f4ecf8922c26d104a63a531e20fd3158b598ce5c
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-02-09 14:21:03 +0530

    intial commit

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -9,7 +9,7 @@ const app = express();
 // should block them with a 404.
 // User will be sending in their user id in the header as 'user-id'
 // You have been given a numberOfRequestsForUser object to start off with which
-// clears every one second
+// clears every one seconds
 
 let numberOfRequestsForUser = {};
 setInterval(() => {
