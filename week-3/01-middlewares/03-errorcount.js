bb9a6bacef4fe6f46a5c6c56c94d2cab0bcb33c2
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-02-09 15:50:54 +0530

    intial commit

--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -23,4 +23,11 @@ app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
 
+app.use(function(err,req,res,next){
+  errorCount = errorCount+1;
+  res.status(404).send({
+    msg: "Error has occurred"
+  })
+})
+
 module.exports = app;
