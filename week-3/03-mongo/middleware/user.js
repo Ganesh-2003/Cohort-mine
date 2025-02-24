e00d4b4d61ecebfb52871a96c8e1426a43a37007
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-02-24 21:22:43 +0530

    intial commit

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,11 @@
+const {User} = require("../db/index")
+
+
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+
+    
 }
 
 module.exports = userMiddleware;
