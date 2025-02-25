fc46493ac12685268b5ef11c975d196e99bc611b
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-02-25 22:59:58 +0530

    intial commit

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,10 +1,36 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User } = require("../db");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const response = await User.findOne({
+        username: username,
+        password: password
+    })
+
+    if(response) {
+        res.status(403).json({
+            msg: "User Already Exists"
+        })
+    }
+    
+    else {
+        User.create({
+            username,
+            password
+        }).then(()=>{
+            res.status(200).json({
+                msg: "User Created Successfully"
+            })
+        })
+    }
+
 });
 
 router.get('/courses', (req, res) => {
