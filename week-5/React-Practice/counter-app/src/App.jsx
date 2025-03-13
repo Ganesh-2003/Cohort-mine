51e72bb61da1d700a828df7f6b9df2ada69b6271
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-03-13 09:23:03 +0530

    intial commit

--- week-5/React-Practice/counter-app/src/App.jsx
+++ week-5/React-Practice/counter-app/src/App.jsx
@@ -8,19 +8,71 @@ import { useState } from 'react'
 //State, components
 
 
-function App() {
+// function App() {
 
-  const[count,setCount] = useState(0); //[1,2]
+//   const[count,setCount] = useState(0); //[1,2]
 
-  function onClickHandler(){
-    setCount(count+1);
-  }
+//   return (
+//     <div>
+//       <CustomButton count={count} setCount={setCount}></CustomButton>
+//     </div>
+//   )
+// }
 
+// function CustomButton(props) {
+
+//   function OnClickHandler() {
+//     props.setCount(props.count+1); 
+//   }
+
+//   return <button onClick = {OnClickHandler}>
+//       Counter {props.count}
+//     </button>
+
+// }
+
+function Todo({title, description}) {
   return (
     <div>
-      <button onClick={onClickHandler}>Counter {count}</button>
+      <h3>{title}</h3>
+      <p>{description}</p>
     </div>
   )
 }
+
+
+
+function App() {
+  const [todos, setTodos] = useState([{
+    title: "Go to Badminton",
+    description: "Go to Badminton between 7-8",
+    completed: false
+  },{
+    title: "Study DSA",
+    description: "Practice Between 7 P.M - 9 P.M",
+    completed: true
+  }])
+
+  function addTodo() {
+    setTodos([...todos,{
+      title: "Random Todo",
+      description: "description for random todo"
+    }])
+  }
+  
+
+  return (
+    <div>
+      <button onClick={addTodo}>Add a random todo</button>
+      <Todo title={todos[0].title} description={todos[0].description}/>
+      <Todo title={todos[1].title} description={todos[1].description}/>
+
+      {todos.map(function(todos){
+        return <Todo title={todos.title} description={todos.description}/>
+      })}
+    </div>
+  )
+}
+
 
 export default App
