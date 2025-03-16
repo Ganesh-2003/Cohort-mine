07276bd4097adfb5dabf8e7d860c0f667b902125
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-03-16 17:03:58 +0530

    Intial commit

---
+++ week-6/Experiments/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+function Header( {title} ) {
+  return <div>
+    {title}
+  </div>
+}
+
+export default App
