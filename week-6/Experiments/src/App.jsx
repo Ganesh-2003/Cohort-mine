ffd96cf700581d57426cce7a4223d276d7ae9966
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-03-23 10:08:59 +0530

    Intial Commit

--- week-6/Experiments/src/App.jsx
+++ week-6/Experiments/src/App.jsx
@@ -3,20 +3,44 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// function App() {
+
+//   return (
+//     <div>
+//       <Header title="Ganesh1">Hi Ganes</Header>
+//       <Header title="Ganesh2"></Header>
+//     </div>
+//   )
+// }
+
+// function Header( props  ) {
+//   return <div>
+//     {props.title}
+//   </div>
+// }
+
+
 function App() {
+
+  const [Name,setName] = useState("Ganesh");
+
+  function Change () {
+    if(Name === "Ganesh") {
+      setName(Math.random().toString())
+    } else {
+      setName("Ganesh");
+    }
+}
 
   return (
     <div>
-      <Header title="Ganesh1">Hi Ganes</Header>
-      <Header title="Ganesh2"></Header>
+    <button onClick={Change}> Click here to change the Title</button>
+    <p> My Name is : {Name}</p>
+    <p> Lives in Chennai</p>
     </div>
   )
 }
 
-function Header( props  ) {
-  return <div>
-    {props.title}
-  </div>
-}
+
 
 export default App
